369da7e781132aec3e99b68e5ad53dfb23633a6c
sabbir <contact.sabbirbhuiyan@gmail.com>
2023-12-26 12:39:35 +0600

    zoom effect has been dynamic

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let img = document.getElementById('img');
 
 
 container.addEventListener('mousemove', (e) => {
-
     const x = e.clientX - e.target.offsetLeft;
     const y = e.clientY - e.target.offsetTop;
     console.log(x + ' ' + y)
@@ -16,3 +15,15 @@ container.addEventListener('mouseleave', () => {
     img.style.transformOrigin = 'center center';
     img.style.transform = 'scale(1)';
 })
+
+const imageUploader = document.querySelector("input");
+
+function showImage() {
+    container.style.display = 'block'
+    let reader = new FileReader();
+    reader.readAsDataURL(imageUploader.files[0]);
+    reader.onload = function(e) {
+        img.classList.add("show");
+        img.src = e.target.result;
+    };
+}
